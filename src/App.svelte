284ca77455596bbf51dcd90609134cfeefced5a2
alasdairhaigh <dev@alasdairhaigh.tech>
2021-04-11 20:58:40 +0100

    Using multiple components

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+	import ContactCard from './ContactCard.svelte';
+	
 	let name = 'Max';
 	let age = 30;
 
@@ -31,3 +33,5 @@
 <!-- <button on:click={changeName}>Change name</button> -->
 <!-- <input type="text" value={name} on:input={nameInput}> -->
 <input type="text" bind:value={name}>
+
+<ContactCard />
